e181ce9c0a2f50e2f39aaaa8d8939c8b7d4b6c18
Luís Silva <luismiguel.s91@gmail.com>
2016-10-30 03:54:27 +0000

    hello test

--- public/index.php
+++ public/index.php
@@ -12,15 +12,24 @@ $default_language = $available_languages[0];
 
 $app = new App();
 $container = $app->getContainer();
-$container['renderer'] = new PhpRenderer("../views/", array("language" => $default_language));
+$container['renderer'] = new PhpRenderer("../views/");
 
 $app->add( new \MultilingualSlim\LanguageMiddleware($available_languages, $default_language, $container) );
 
 $app->get('/', function (Request $request, Response $response) {
 
-		return $this->renderer->render($response, "base.php", [
-			"language" => $this->language
-		]);
+        return $this->renderer->render($response, "base.php", [
+            "language" => $this->language
+        ]);
+});
+
+$app->get('/hello', function (Request $request, Response $response) {
+
+        if ($this->language === $this->default_language || $this->language === 'pt') {
+            return $response->write("Olá");
+        } else {
+            return $response->write("Hello");
+        }
 });
 
 $app->run();
